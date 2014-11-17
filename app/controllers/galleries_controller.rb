e443170615fb43c012f8297dd1f08a30b00222d1
RDegnen <rdegnen@gmail.com>
2014-11-17 11:57:19 -0500

    Added div tags and inline blocks to galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,7 +4,7 @@ class GalleriesController < ApplicationController
 		@response = RestClient.get('https://api.imgur.com/3/gallery/r/pics/', Authorization: 'Client-ID 96b7bf7f245f91e')
 		response_hash = JSON.parse(@response)
 		@parsed = response_hash["data"]
-		@mapped = @parsed.map { |k| [(k['title'] + '---' + k['link'])]}
+		@mapped = @parsed.map { |k| [k['title'], k['link']] }
 	end
 
 
